bd0a2d02bf1f18b698365a097b328bc8b8251f07
FORMAT1 <olayiwolafm@gmail.com>
2023-03-20 18:21:58 +0000

    6. DBStorage - States and Cities

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ New engine DBStorage """
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session, session, scoped_session, relationship
 from models.base_model import BaseModel, Base
 from models.user import User
 from models.place import Place
@@ -9,62 +9,67 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
-import os
+from os import getenv
 
 
-classes = {
-    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-    'State': State, 'City': City, 'Amenity': Amenity,
-    'Review': Review
-}
-
-
-class DBStorage:
+class DBStorage():
     """ DBStorage Class """
     __engine = None
     __session = None
 
     def __init__(self):
         """ init method """
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
-            os.getenv('HBNB_MYSQL_USER'),
-            os.getenv('HBNB_MYSQL_PWD'),
-            os.getenv('HBNB_MYSQL_HOST'),
-            os.getenv('HBNB_MYSQL_DB')), pool_pre_ping=True)
+        mysql_user = getenv('HBNB_MYSQL_USER')
+        mysql_pwd = getenv('HBNB_MYSQL_PWD')
+        mysql_host = getenv('HBNB_MYSQL_HOST')
+        mysql_db = getenv('HBNB_MYSQL_DB')
+        mysql_env = getenv('HBNB_ENV') 
 
-        if os.getenv('HBNB_ENV') == 'test':
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+            mysql_user, mysql_pwd, mysql_host, mysql_db), pool_pre_ping=True)
+
+        if mysql_env == 'test'
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         """ all method """
-        dict_objs = {}
-        for clss in classes:
-            if cls is None or cls is classes[clss] or cls is clss:
-                objs = self.__session.query(classes[clss]).all()
-                for obj in objs:
-                    key = obj.__class__.__name__ + '.' + obj.id
-                    dict_objs[key] = obj
-        return (dict_objs)
+        if cls:
+            objs = self.__session.query(cls).all()
+        else:
+            classes = [State, City, User, Place, Review, Amenity]
+            objs = []
+            for _class in classes:
+                objs += self.__session.query(_class)
+        """to create and save data"""
+        new_dict = {}
+    
+        for obj in objs:
+            key = '{}.{}'.format(type(obj).__name__, obj.id)
+            new_dict[key] = obj
+        return new_dict
 
     def new(self, obj):
         """ new method """
         self.__session.add(obj)
 
     def save(self):
-        """ save method """
+        """ save method to commit all changes """
         self.__session.commit()
 
     def delete(self, obj=None):
-        """ delete method """
+        """ delete method to delete from the 
+            current database
+        """
         if obj:
             self.__session.delete(obj)
             self.save()
 
     def reload(self):
-        """ reload method """
+        """to create all tables in the database"""
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine,
+        self.__session = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
+        """to create current session in the db"""
         Session = scoped_session(session_factory)
         self.__session = Session()
 
--- models/state.py
+++ models/state.py
@@ -2,11 +2,25 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, foreignkey
-from sqlalchemy.orm import relationship
-
+from sqlalchemy.orm import relationship, backref
+import models
+from os import getenv
+from models.city import City
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    stat_id = Column(String(60), nullable=False, foreignkey=("state.id"))
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade="all, delete", backref="state")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """Returns list of city"""
+            the_cities = []
+            for c in models.storage.all(City).values():
+                if c.state_id == self.id:
+                    the_cities.append(c)
+            return the_cities
