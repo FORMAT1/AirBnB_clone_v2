6bb4b927825fb634ac6cfaaa85968199334c627e
FORMAT1 <olayiwolafm@gmail.com>
2023-04-23 07:36:27 +0000

    HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,13 +6,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
 """returns Hello HBNB"""
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
 """returns HBNB"""
     return 'HBNB'
